12377b2baeb623571720438caf859378a50d38f0
yanrepo <yan23mail@gmail.com>
2022-12-02 22:30:35 +0300

    init

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import { useState } from 'react';
+import './App.css';
+
+export default function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div className="App">
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+      </div>
+    </div>
+  );
+}
