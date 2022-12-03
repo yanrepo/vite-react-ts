9a8381787ffec4a68e661083783314d91aa79155
yanrepo <yan23mail@gmail.com>
2022-12-03 23:23:44 +0300

    input note

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,49 @@
 import './App.css';
+import { useState } from 'react';
+import initStateNotes from './initStateNotes.json';
 
 export default function App() {
-  return <div className="App"></div>;
+  const [notes, setNotes] = useState(initStateNotes);
+  const [form, setForm] = useState('');
+
+  const handleChange = (
+    e: React.ChangeEvent<HTMLInputElement>
+  ): void => {
+    setForm(e.currentTarget.value);
+  };
+
+  const handleSubmit = (e: React.FormEvent): void => {
+    e.preventDefault();
+    const trimmed: string = form.trim();
+    if (trimmed !== '') {
+      const date: string = new Date().toLocaleString();
+      const newNote = {
+        date: date,
+        note: trimmed,
+      };
+      setNotes([newNote, ...notes]);
+    }
+    setForm('');
+  };
+
+  return (
+    <div className="App">
+      <form onSubmit={handleSubmit}>
+        <input type="text" value={form} onChange={handleChange} />
+        <input type="submit" value="Save" />
+      </form>
+
+      <ul>
+        {notes.map((note, index) => {
+          return (
+            <li key={index}>
+              <p>
+                {note.note} ~ {note.date}
+              </p>
+            </li>
+          );
+        })}
+      </ul>
+    </div>
+  );
 }
